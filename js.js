11d8dfc9a538e50d0a438f588046a4233de8a05e
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-19 16:24:30 +0100

    sololearn 1

---
+++ js.js
@@ -0,0 +1,72 @@
+// var ehsan = "my 'ggg \"\"\f name is Ehsan";
+
+// document.write('hallo' + ' ' + ehsan);
+// console.log('hallo' + ' ' + ehsan)
+
+// var nm='10'/ 3;
+// console.log(typeof(nm));
+// console.log(nm)
+
+// var a= 0,b=10;
+// var a= b++;
+// console.log(a)
+// console.log(b)
+
+// var a= (10+5)*3;
+// console.log(a)
+
+// let x = 3;
+// let y = 2;
+// x %= y;
+// console.log(x +=2 +=2 )
+
+// console.log((2==2)&&(3==3));
+
+// var first = "ehsan "
+// var last = "bazaei "
+// console.log(first + last)
+
+
+// function main() {
+//     var distance = parseInt(readLine(), 10);
+//         //your code goes here
+//         var speed = 40;
+//         var minutes = distance / speed;
+//        console.log(minutes * 60);
+// }
+
+// let a = 6;
+// let b = 4;
+// if(a>b){
+//     alert("nee")
+// }
+
+
+// if(1>5){
+//     document.write("a");
+// }else if(2<4){
+//     document.write("b")
+// }else{
+//     document.write("c")
+// }
+
+var grade='B';
+switch (grade)
+{
+   case 'A': console.log("Good job<br />");
+   break;
+
+   case 'B': console.log("Pretty good<br />");
+   break;
+
+   case 'C': console.log("Passed<br />");
+   break;
+
+   case 'D': console.log("Not so good<br />");
+   break;
+
+   case 'F': console.log("Failed<br />");
+   break;
+
+   default:  console.log("Unknown grade<br />")
+}
